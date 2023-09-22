15efd27176050bc7d9d515984d6daea205e23ee7
Noah <noah95mitchell@gmail.com>
2023-09-21 19:08:40 -0700

    minimally functional version

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-import json
-import os
 from flask import Flask, render_template, request, jsonify
 import openai
-from openai.error import RateLimitError
+import os
 
 app = Flask(__name__)
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+prompt = "The following body of text is a company's privacy policy outlining how they handle personal and private information from a consumer. Outline key points and present them in a short and succinct bullet list. Focus on privacy infringements and shady practices. The 5 main bullet point topics to cover are what specific information is being collected on the user, how this information is stored, who this information is shared with, whether profit is made from this information and/or it is sold and how long this information is stored for, along with whether you can request for its deletion. Please provide a sixth bullet point describe the most invasively collected data. Please provide examples of the most egregious and privacy breaking data collections outlined in the policy. The company's privacy policy is as follows: "
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index2.html')
 
-@app.route('/gpt4', methods=['GET', 'POST'])
-def gpt4():
-    user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
-    
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    print('query called')
+    '''
+    if request.method == 'GET':
+        textInput = request.args.get('textInput')
+    elif request.method == 'POST':
+        textInput = request.form['textInput']
+    '''
+    textInput = request.args.get('textInput') if request.method == 'GET' else request.form['textInput']
+
+    messages = [{"role": "user", "content": (textInput)}]
 
     try:
+        print('sending')
         response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model='gpt-4', 
             messages=messages
         )
         content = response.choices[0].message["content"]
-    except RateLimitError:
-        content = "The server is experiencing a high volume of requests. Please try again later."
+    except openai.error.RateLimitError:
+        content = "Rate limit has been reached, please try again later."
+    except:
+        content = "Error has occured, please contact administrator."
 
     return jsonify(content=content)
 
---
+++ readme.txt
@@ -0,0 +1,1 @@
+template code adopted and editted from https://medium.com/codingthesmartway-com-blog/how-to-create-an-ai-powered-python-web-app-with-flask-and-gpt-4-api-907dcffd1221
