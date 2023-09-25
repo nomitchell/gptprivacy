b47d135e2c959233310883b89105bb887013de69
Noah Mitchell <noah@gorobmitchell.com>
2023-09-25 19:59:36 -0400

    updated css

--- test.py
+++
@@ -1,5 +0,0 @@
-import trafilatura
-
-downloaded = trafilatura.fetch_url('https://www.amazon.com/gp/help/customer/display.html?nodeId=GX7NJQ4ZB8MHFRNJ')
-text = trafilatura.extract(downloaded)
-print(text)
